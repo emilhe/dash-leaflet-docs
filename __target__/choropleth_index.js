d1ff6a20b3d0caf30b58d8cb5dc88ae18be3c44d
emher <emil.h.eriksen@gmail.com>
2020-09-03 21:01:54 +0200

    clean

--- __target__/choropleth_index.js
+++
@@ -1,7 +0,0 @@
-import {discrete} from './choropleth.js'
-
-window.dash_props = Object.assign({}, window.dash_props, {
-    choropleth: {
-        discrete
-    }
-});
--- __target__/scatter_index.js
+++
@@ -1,7 +0,0 @@
-import {_get_color,_resolve_options,bind_popup,cluster_to_layer,point_to_layer} from './scatter.js'
-
-window.dash_props = Object.assign({}, window.dash_props, {
-    scatter: {
-        _get_color,_resolve_options,bind_popup,cluster_to_layer,point_to_layer
-    }
-});
